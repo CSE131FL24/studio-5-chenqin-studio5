7f2b5bd03db28a94cfd6d7eee105227289808047
Anthony Qin <anthony.qin9@gmail.com>
2024-10-16 14:05:34 -0500

    finished studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -16,6 +16,7 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
+		distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 		
 		return distance;
 	}
@@ -34,17 +35,19 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, 0.75 * radius);
 		
-
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(145, 0, 0);
+		StdDraw.filledCircle(x, y, 0.5 * radius);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, 0.25 * radius);
 		
 	}
 
@@ -75,7 +78,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
 		return sum;
 	}
 
@@ -89,7 +94,13 @@ public class Methods {
 	public static int[] filledArray(int length, int value) {
 		int[] values = null; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
-
+		
+		values = new int[length];
+		for (int i =0; i < length; i++) {
+			values[i] = value;
+		}
+		
+		
 		
 
 		return values;
@@ -98,5 +109,21 @@ public class Methods {
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
 	// TODO: Create a JavaDoc comment for the arrayMean method.
 
+/**
+ * 
+ * Return the average of all the integers in an array with a provided array of integers
+ * 	
+ * @param values - an array of integers
+ * @return mean - the average of the integers
+ */
+	public static double arrayMean(int[] values) {
+		double sum = 0; 
+		
+		for (int i = 0; i < values.length; i++) {
+			sum += values[i];	
+		}
+		double mean = sum / values.length;
+		return mean;
+	}
 	
 }
